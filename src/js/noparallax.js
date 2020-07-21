bc7d6f21c08682567c240be4be6389333a59fad9
Thomas Plucinski <>
2020-07-21 11:45:40 +0200

    Adjust night sky with gaussian curve for star brightness

--- src/js/noparallax.js
+++ src/js/noparallax.js
@@ -9,7 +9,8 @@ function createStar({x, y}, index, debug) {
     const starTranslate = document.createElementNS("http://www.w3.org/2000/svg", 'g');
     starTranslate.setAttribute('transform', `translate(${x} ${y})`);
 
-    const radius = debug ? 10 : 1;
+    // const radius = (Math.random() + Math.random() + Math.random()) / 3 * 2 + 0.5;
+    const radius = randn_bm(0.5, 4, 2.7);
     const delay = index * 100 + 500 * Math.random();
     const duration = 3000 + Math.random() * 4000;
     const brightness = 0.9 + Math.random() * 0.1;
@@ -54,4 +55,18 @@ function generateStarData(numStars) {
     }
 
     return stars
+}
+
+function randn_bm(min, max, skew) {
+    let u = 0, v = 0;
+    while(u === 0) u = Math.random(); //Converting [0,1) to (0,1)
+    while(v === 0) v = Math.random();
+    let num = Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
+
+    num = num / 10.0 + 0.5; // Translate to 0 -> 1
+    if (num > 1 || num < 0) num = randn_bm(min, max, skew); // resample between 0 and 1 if out of range
+    num = Math.pow(num, skew); // Skew
+    num *= max - min; // Stretch to fill range
+    num += min; // offset to min
+    return num;
 }
